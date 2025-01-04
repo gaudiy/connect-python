8dcbf2f886cf40463f186658f2de6a7abf367a15
tsubakiky <salovers1205@gmail.com>
2025-01-04 19:28:34 +0900

    testdata: remove ping middlware setting func

--- tests/testdata/ping/v1/v1connect/ping_connect.py
+++ tests/testdata/ping/v1/v1connect/ping_connect.py
@@ -1,16 +1,13 @@
 """Module provides the implementation for the ping service."""
 
 import abc
-from collections.abc import Callable, Coroutine
 from enum import Enum
-from typing import Any
 
 from google.protobuf.descriptor import MethodDescriptor, ServiceDescriptor
 
 from connect.handler import UnaryHandler
-from connect.options import ConnectOptions
-from connect.request import ConnectRequest, Request
-from connect.response import ConnectResponse, Response
+from connect.request import ConnectRequest
+from connect.response import ConnectResponse
 from tests.testdata.ping.v1 import ping_pb2
 from tests.testdata.ping.v1.ping_pb2 import PingRequest, PingResponse
 
@@ -31,25 +28,6 @@ class PingServiceHandler(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     async def Ping(self, request: ConnectRequest[PingRequest]) -> ConnectResponse[PingResponse]: ...
-
-
-def add_PingService_to_handler(
-    handler: PingServiceHandler, options: ConnectOptions | None = None
-) -> Callable[..., Coroutine[Any, Any, Response]]:
-    """Add the ping service to the handler."""
-    pingServicePing_handler = UnaryHandler(
-        PingServiceProcedures.Ping.value, handler.Ping, PingRequest, PingResponse, options
-    )
-
-    async def handle(request: Request) -> Response:
-        path = request.url.path
-        match path:
-            case PingServiceProcedures.Ping.value:
-                return await pingServicePing_handler.handle(request)
-            case _:
-                raise NotImplementedError(f"Path {path} not implemented")
-
-    return handle
 
 
 def create_PingService_handlers(service: PingServiceHandler) -> list[UnaryHandler]:

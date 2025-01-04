91eb776a32cc226f699610de32a3e0fe498d1516
Koichi Shiraishi <zchee.io@gmail.com>
2025-01-04 19:45:15 +0900

    cmd/protoc-gen-connect-python: initialize generator (#6)
    * cmd/protoc-gen-connect-python: initialize generator
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>
    
    * all: genelate
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>
    
    ---------
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cmd/protoc-gen-connect-python/main.go
+++ cmd/protoc-gen-connect-python/main.go
@@ -4,7 +4,31 @@
 package main
 
 import (
-	_ "google.golang.org/protobuf/proto"
+	"flag"
+	"fmt"
+	"os"
+
+	"google.golang.org/protobuf/compiler/protogen"
+
+	"github.com/gaudiy/connect-python/cmd/protoc-gen-connect-python/generator"
 )
 
-func main() {}
+var pluginVersion = "devel"
+
+func main() {
+	if len(os.Args) == 2 && os.Args[1] == "--version" {
+		fmt.Fprintln(os.Stdout, pluginVersion)
+		os.Exit(0)
+	}
+
+	var f flag.FlagSet
+	var cfg generator.Config
+	protogen.Options{ParamFunc: f.Set}.Run(func(plugin *protogen.Plugin) error {
+		gen, err := generator.NewGenerator(plugin, &cfg)
+		if err != nil {
+			return err
+		}
+		gen.Generate()
+		return nil
+	})
+}

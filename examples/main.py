59397261e5a7ebed0c79e33584cbc9696292e138
tsubakiky <salovers1205@gmail.com>
2025-01-04 23:21:41 +0900

    pyproject: update pkg

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,10 @@
 """Main module for the tests."""
 
 import os
+from typing import Any
 
 from connect.app import ConnectASGI
-from connect.connect import ConnectRequest, ConnectResponse, Req, Res
+from connect.connect import ConnectRequest, ConnectResponse
 from connect.interceptor import Interceptor, UnaryFunc
 from connect.options import ConnectOptions
 
@@ -23,10 +24,10 @@ class ElizaService(ElizaServiceHandler):
 class IPRestrictionInterceptor(Interceptor):
     """IP restriction interceptor."""
 
-    async def wrap_unary(self, next: UnaryFunc[Req, Res]) -> UnaryFunc[Req, Res]:
+    def wrap_unary(self, next: UnaryFunc) -> UnaryFunc:
         """Wrap a unary function with the interceptor."""
 
-        async def _wrapped(request: ConnectRequest[Req]) -> ConnectResponse[Res]:
+        async def _wrapped(request: ConnectRequest[Any]) -> ConnectResponse[Any]:
             white_list = os.environ.get("WHITE_LIST", "").split(",")
             if not white_list:
                 raise Exception("White list not found")
@@ -36,7 +37,7 @@ class IPRestrictionInterceptor(Interceptor):
                 raise Exception("Address not found")
 
             ip = address.host
-            if ip != "":
+            if ip == "":
                 raise Exception("IP not allowed")
 
             if ip not in white_list:

2e041c97e8cde9702d2c0a714b7091a17e502178
Koichi Shiraishi <zchee.io@gmail.com>
2025-02-21 05:02:08 +0900

    examples/cmd/examples-go: refactor
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- examples/cmd/examples-go/client/main.go
+++ examples/cmd/examples-go/client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,70 +12,134 @@ import (
 	"github.com/gaudiy/connect-python/examples/proto/connectrpc/eliza/v1/v1connect"
 )
 
-// func main() {
-// 	client := v1connect.NewElizaServiceClient(
-// 		http.DefaultClient,
-// 		"http://localhost:8080/",
-// 	)
-// 	req := connect.NewRequest(&elizav1.SayRequest{
-// 		Sentence: "Hi",
-// 	})
-// 	req.Header().Set("Some-Header", "hello from connect")
-// 	res, err := client.Say(context.Background(), req)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	log.Println(res.Msg)
-// 	log.Println(res.Header())
-// }
+type mode int
 
-// func main() {
-// 	client := v1connect.NewElizaServiceClient(
-// 		http.DefaultClient,
-// 		"http://localhost:8080/",
-// 	)
+const (
+	modeClient mode = iota + 1
+	// for future use?
+	modeServer
+)
 
-// 	stream := client.IntroduceClient(context.Background())
-// 	for i := 0; i < 5; i++ {
-// 		err := stream.Send(&elizav1.IntroduceRequest{
-// 			Name: "Alice",
-// 		})
-// 		if err != nil {
-// 			log.Fatalln(err)
-// 			break
-// 		}
-// 	}
+type rpc int
 
-// 	res, err := stream.CloseAndReceive()
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
+const (
+	unary rpc = iota + 1
+	serverStreaming
+	clientStreaming
+	bidirectionalStreaming
+)
 
-// 	log.Println(res.Msg)
-// }
+var (
+	runMode mode
+	rpcType rpc
+)
+
+func init() {
+	// mode
+	clientMode := flag.Bool("c", false, "run client mode")
+	serverMode := flag.Bool("s", false, "run server mode")
+	// rpc
+	unaryRPC := flag.Bool("u", false, "send unary RPC")
+	serverStreamingRPC := flag.Bool("ss", false, "send unary RPC")
+	clientStreamingRPC := flag.Bool("cs", false, "send unary RPC")
+	bidiRPC := flag.Bool("bidi", false, "send unary RPC")
+
+	flag.Parse()
+
+	switch {
+	case *clientMode:
+		runMode = modeClient
+	case *serverMode:
+		runMode = modeServer
+	default: // throw
+		if !*clientMode && !*serverMode {
+			log.Fatal("either clientMode or serverMode must be enabled. [-c, -s]")
+		}
+		if *clientMode && *serverMode {
+			log.Fatal("neither clientMode nor serverMode can't be enabled")
+		}
+		panic("unreachable")
+	}
+
+	switch {
+	case *unaryRPC:
+		rpcType = unary
+	case *serverStreamingRPC:
+		rpcType = serverStreaming
+	case *clientStreamingRPC:
+		rpcType = clientStreaming
+	case *bidiRPC:
+		rpcType = bidirectionalStreaming
+	default: // throw
+		log.Fatal("please enable any rpc type. [-u, -ss, -cs, -bidi]")
+	}
+}
 
 func main() {
-	client := v1connect.NewElizaServiceClient(
-		http.DefaultClient,
-		"http://localhost:8080/",
-	)
+	switch runMode {
+	case modeClient:
+		if err := runClient(); err != nil {
+			log.Fatal(err)
+		}
+	case modeServer:
+		// TODO(tsubakiky): not implemented yet
+	}
+}
 
-	request := connect.NewRequest(&elizav1.IntroduceRequest{
-		Name: "Alice",
-	})
-	stream, err := client.IntroduceServer(context.Background(), request)
-	if err != nil {
-		log.Fatalln(err)
+func runClient() error {
+	client := v1connect.NewElizaServiceClient(http.DefaultClient, "http://localhost:8080/")
+
+	ctx := context.Background()
+	switch rpcType {
+	case unary:
+		req := connect.NewRequest(&elizav1.SayRequest{
+			Sentence: "Hi",
+		})
+		req.Header().Set("Some-Header", "hello from connect")
+		res, err := client.Say(ctx, req)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("res.Msg: %v\n", res.Msg)
+		fmt.Printf("res.Header(): %v\n", res.Header())
+
+	case clientStreaming:
+		stream := client.IntroduceClient(ctx)
+		for range 5 {
+			err := stream.Send(&elizav1.IntroduceRequest{
+				Name: "Alice",
+			})
+			if err != nil {
+				return err
+			}
+		}
+		res, err := stream.CloseAndReceive()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("res.Msg: %v\n", res.Msg)
+
+	case serverStreaming:
+		request := connect.NewRequest(&elizav1.IntroduceRequest{
+			Name: "Alice",
+		})
+		stream, err := client.IntroduceServer(ctx, request)
+		if err != nil {
+			return err
+		}
+		defer stream.Close()
+
+		for number := int64(1); stream.Receive(); number++ {
+			fmt.Printf("Received message %d: %s\n", number, stream.Msg().Sentence)
+		}
+
+		if err := stream.Err(); err != nil {
+			return err
+		}
+
+	case bidirectionalStreaming:
+		// TODO(tsubakiky): no-op yet
 	}
 
-	number := int64(1)
-	for ; stream.Receive(); number++ {
-		log.Printf("Received message %d: %s", number, stream.Msg().Sentence)
-	}
-
-	if err := stream.Err(); err != nil {
-		log.Fatalln(err)
-	}
-
-	stream.Close()
+	return nil
 }
